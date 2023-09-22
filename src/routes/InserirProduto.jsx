c074e73d68b99c9b038c7cd33d3d5dec75168443
Nanre1027 <amorimrenan597@gmail.com>
2023-09-22 09:41:43 -0300

    Criando o CadastrarProduto

--- src/routes/InserirProduto.jsx
+++ src/routes/InserirProduto.jsx
@@ -3,11 +3,56 @@ import { ListaProdutos } from "../components/ListaProdutos";
 export default function InserirProduto() {
     document.title = "Cadastrar";
 
+    const navigate = userNavigate();
+
     let novoId = ListaProdutos[ListaProdutos.length - 1]
 
+    const [produto, setProduto] = useState({
+        id:novoid,
+        nome:"",
+        desc:"",
+        valor:""
+    });
+
+    const handleChange = (e)=>[
+        e.preventDefault();
+        const[name,value] = e.target;
+        setProduto({...produto,[name]:value});
+        }
+    ]
+    const handleSubmit = (e)=>[
+        e.preventDefault();
+        ListaProdutos.push(produto);
+        navigate("/produtos");
+    ]
     return (
         <div>
-            <h1>Cadastro de Produtos</h1>
+            <h1>CADASTRAR PRODUTOS</h1>
+    
+            <div>
+              <form onSubmit={handleSubmit}>
+                <fieldset>
+                  <legend>PRODUTO</legend>
+                  <div>
+                    <input type="hidden" name="id" value={produto.id} onChange={handleChange}/>
+                  </div>
+                  <div>
+                    <label htmlFor="idNome">Nome do Produto:</label>
+                    <input type="text" name="nome" id="idNome" value={produto.nome} onChange={handleChange}/>
+                  </div>
+                  <div>
+                    <label htmlFor="idDesc">Descrição do Produto:</label>
+                    <input type="text" name="desc" id="idDesc" value={produto.desc} onChange={handleChange}/>
+                  </div>
+                  <div>
+                    <label htmlFor="idValor">Valor do Produto:</label>
+                    <input type="text" name="valor" id="idValor" value={produto.valor} onChange={handleChange}/>
+                  </div>
+                  <div>
+                    <button>CADASTRAR</button>
+                  </div>
+                </fieldset>
+              </form>
+            </div>
         </div>
-    )
-}
+      )
