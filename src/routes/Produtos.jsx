d48e613986fd452adf61b22d712c150da53e323a
luizPrendin <luizprendinfelipe@gmail.com>
2023-09-21 21:05:54 -0300

    atualizando branch

--- src/routes/Produtos.jsx
+++ src/routes/Produtos.jsx
@@ -39,17 +39,16 @@ export default function Produtos() {
       <div>
         <button className={style.buttonStyle} onClick={()=> setCounter2(counter2 + 1)}>COUNTER2 - {counter2}</button>
       </div>
-      <div>
-        <button className={style.buttonStyle} > <Link to={`/adicionar/produtos`}>Adicionar Produtos</Link></button>
-      </div>
+      
       <table className={style.tblEstilo}>
         <thead>
         <tr>
           <th>ID</th>
           <th>NOME</th>
           <th>DESCRIÇÃO</th>
-          <th>PREÇO</th>
-          <th>EDITAR / EXCLUIR</th>       
+          <th>PREÇO </th>
+          <th>EDITAR </th>
+          <th>EXCLUIR</th>       
         </tr>
         </thead>
         <tbody>
@@ -60,14 +59,17 @@ export default function Produtos() {
                 <td>{item.nome}</td>
                 <td>{item.desc}</td>
                 <td>{item.valor}</td>
-                <td> <Link to={`/editar/produtos/${item.id}`}><Editar/></Link> | <Link to={`/excluir/produtos/${item.id}`}><Excluir/></Link> </td>
+                <td> <Link to={`/editar/produtos/${item.id}`}><Editar/></Link>   </td>
+                <td><Link to={`/excluir/produtos/${item.id}`}><Excluir/></Link></td>
             </tr>
           ))
         }
 </tbody>
 <tfoot>
   <tr>
-    <td colSpan={4}>PRODUTOS LINDOS</td>
+    <td colSpan={4}><div>
+        <button className={style.buttonStyle} > <Link to={`/adicionar/produtos`}style={{ textDecoration: 'none' }}>Adicionar Produtos</Link></button>
+      </div></td>
   </tr>
 </tfoot>
       </table>
